78e12c25a11f8fc768ba4bfc4ad7a42ecd7caf0a
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-11-17 21:01:16 -0500

    adding event listeners

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,9 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+
+//var todaydate = daysjs();
+//$("#currentDay").text(todaydate.format("MMMM Do, YYYY"));
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -21,3 +24,14 @@ $(function () {
   //
   // TODO: Add code to display the current date in the header of the page.
 });
+
+var savebtns = $(".saveBtn");
+
+for (var btn of savebtns) {
+  $(btn).click(function (event) {
+    var target = event.target;
+    var parent = $(target).parent();
+    var text = $(parent).find(".description");
+    console.log(text);
+  });
+}
