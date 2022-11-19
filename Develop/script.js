b6ad26adcafad0005edd1935d360a7172748a050
Hovig Kalayjian <hovigkalayjian@gmail.com>
2022-11-18 20:51:17 -0500

    adding time block comparisons but still not working

--- Develop/script.js
+++ Develop/script.js
@@ -2,11 +2,37 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
+var today = dayjs();
+$("#currentDay").text(today.format("MMM D, YYYY"));
+
 //$(document).ready(function () {
 //for (var i = 9; i < 18; i++) {}
 //});
-var today = dayjs();
-$("#currentDay").text(today.format("MMM D, YYYY"));
+
+function timeTracker() {
+  //get current number of hours.
+  var timeNow = moment().hour();
+
+  // loop over time blocks
+  $(".time-block").each(function () {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+
+    // To check the time and add the classes for background indicators
+    if (blockTime < timeNow) {
+      $(this).removeClass("future");
+      $(this).removeClass("present");
+      $(this).addClass("past");
+    } else if (blockTime === timeNow) {
+      $(this).removeClass("past");
+      $(this).removeClass("future");
+      $(this).addClass("present");
+    } else {
+      $(this).removeClass("present");
+      $(this).removeClass("past");
+      $(this).addClass("future");
+    }
+  });
+}
 $(function () {
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
